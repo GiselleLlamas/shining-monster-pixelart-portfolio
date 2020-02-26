ae94554b5c3a6208c55a875f792a6cb3aeec6c36
Giselle Llamas <giselle.llamas@gmail.com>
2020-02-26 00:19:41 -0300

    Maquetado de CV
    Maquetado de CV y agregado Galeria.

--- js/funciones.js
+++ js/funciones.js
@@ -4,3 +4,59 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var imgActual = 1;
+		
+function textoAparece(texto){
+	varTexto = '#' + texto;
+	//console.log(varTexto)
+	$(varTexto).fadeIn('fast');
+}
+
+function textoDesaparece(texto){
+	varTexto = '#' + texto;
+	//console.log(varTexto)
+	$(varTexto).fadeOut('fast');
+}
+
+function prevImg(varImg){
+	contenidoHtml = '';
+	if(varImg == 1){
+		contenidoHtml = '<img src="img/img' + 9 + '.png" alt="imagen" />';
+		imgActual = 9;
+	}else{
+		varImg = varImg - 1;
+		contenidoHtml = '<img src="img/img' + varImg + '.png" alt="imagen" />';
+		imgActual = imgActual - 1;
+	}
+	document.getElementById('contenedorFotoPopUp').innerHTML = '';
+	document.getElementById('contenedorFotoPopUp').innerHTML = contenidoHtml;
+
+}
+
+function nextImg(varImg){
+	contenidoHtml = '';
+	if(varImg == 9){
+		contenidoHtml = '<img src="img/img' + 1 + '.png" alt="imagen" />';
+		imgActual = 1;
+	}else{
+		varImg = varImg + 1;
+		contenidoHtml = '<img src="img/img' + varImg + '.png" alt="imagen" />';
+		imgActual = imgActual + 1;
+	}
+	document.getElementById('contenedorFotoPopUp').innerHTML = '';
+	document.getElementById('contenedorFotoPopUp').innerHTML = contenidoHtml;
+}
+
+function apareceGaleria(varImg){
+	$('#fondoGaleria').fadeIn('fast');
+	imgActual = varImg;
+	contenidoHtml = '<img src="img/img' + imgActual + '.png" alt="imagen" />';
+	document.getElementById('contenedorFotoPopUp').innerHTML = '';
+	document.getElementById('contenedorFotoPopUp').innerHTML = contenidoHtml;
+	$('#popUpGaleria').fadeIn('slow');
+}
+function desapareceGaleria(){
+	$('#fondoGaleria').fadeOut('slow');
+	$('#popUpGaleria').fadeOut('fast');
+}
